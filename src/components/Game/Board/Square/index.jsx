600b1080180801ede1dfa5d4c7736b82e70b1f9f
Kuznetsov Yaroslav <kuznetsov.yaroslav.yk@gmail.com>
2017-02-10 11:54:24 +0200

    Square popovers

--- src/components/Game/Board/Square/index.jsx
+++ src/components/Game/Board/Square/index.jsx
@@ -1,5 +1,8 @@
 import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
+import {Popover, OverlayTrigger} from 'react-bootstrap';
+
+
 
 class Square extends Component {
 
@@ -8,9 +11,21 @@ class Square extends Component {
     }
 
     render() {
+        //@todo change UI...
+        const cardData = (
+              <Popover title="" id={"square-"+this.props.index}>
+                    <div className="row" style={{backgroundColor:this.props.color, height: "10px"}}></div>
+                    <div className="row"><strong>{this.props.name}</strong></div>
+                    <div className="row"><span>{`$ ${this.props.price}`||''}</span></div>
+              </Popover>
+        );
 
-        console.log(this.props);
-        return (<p>{this.props.name}</p>);
+
+        return (
+                <OverlayTrigger trigger={['hover', 'focus']} placement="bottom" overlay={cardData}>
+                    <p>{this.props.name}</p>
+                </OverlayTrigger>
+        );
     }
 }
 
