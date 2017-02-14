a873f22c060b3114e36805195dd6e7532a6c5538
Kuznetsov Yaroslav <kuznetsov.yaroslav.yk@gmail.com>
2017-02-14 18:28:53 +0200

    squares connected to players

--- src/components/Game/Board/ControlBoard/index.jsx
+++ src/components/Game/Board/ControlBoard/index.jsx
@@ -1,10 +1,12 @@
 import React, { Component, PropTypes } from 'react';
 import {Button} from 'react-bootstrap';
+import  * as playerActions  from 'redux/actions/playerRowActions';
 import  * as gameActions  from 'redux/actions/gameActions';
+import  * as popupActions  from 'redux/actions/popupActions';
 import { connect } from 'react-redux';
 import Dice from '../Dice';
 import {Player} from 'components/Game/Player';
-
+import Popup from '../Popup';
 import Alert from '../Alert';
 
 
@@ -18,11 +20,7 @@ class ControlBoard extends Component {
             option:             false, 
             buy:                false,
             manage:             false,
-            landed:             {},
-            nextButton:         {},
             showResignbutton:   false, 
-            showNextbutton:     false,
-            showLanded:         false
         }
     }
 
@@ -38,11 +36,162 @@ class ControlBoard extends Component {
 
     updateMoney  = () => {
         let p = this.props.playersConfig.players[this.props.game.currentPlayer];
-        if(p.money < 0 )    this.setState({showResignbutton: true, showNextbutton: false});
-        else                this.setState({showResignbutton: false, showNextbutton: true});
+        if(p.money < 0 ) {   
+            this.setState({showResignbutton: true});
+            let nextButton={...this.props.game.nextButton, show:false};
+            this.props.dispatch(gameActions.setNextButton(nextButton)); 
+        } else {                
+            this.setState({showResignbutton: false});
+            let nextButton={...this.props.game.nextButton, show:true};
+            this.props.dispatch(gameActions.setNextButton(nextButton));  
+        }
 
-        if(this.state.landed.data == "") this.setState({showLanded: false});
+        if(this.props.game.landed.text == "") this.props.dispatch(gameActions.setLanded({show:false}));
     }
+
+    updatePosition = () => {
+
+        this.props.dispatch(gameActions.updatePlayerToSquare({
+            player: this.props.game.currentPlayer,
+            square: this.props.playersConfig.players[this.props.game.currentPlayer].position
+        }));
+
+        // Reset borders
+        // document.getElementById("jail").style.border = "1px solid black";
+        // document.getElementById("jailpositionholder").innerHTML = "";
+        // for (var i = 0; i < 40; i++) {
+        //     document.getElementById("cell" + i).style.border = "1px solid black";
+        //     document.getElementById("cell" + i + "positionholder").innerHTML = "";
+
+        // }
+
+        // var sq, left, top;
+
+        // for (var x = 0; x < 40; x++) {
+        //     sq = square[x];
+        //     left = 0;
+        //     top = 0;
+
+        //     for (var y = turn; y <= pcount; y++) {
+
+        //         if (player[y].position == x && !player[y].jail) {
+
+        //             document.getElementById("cell" + x + "positionholder").innerHTML += "<div class='cell-position' title='" + player[y].name + "' style='background-color: " + player[y].color + "; left: " + left + "px; top: " + top + "px;'></div>";
+        //             if (left == 36) {
+        //                 left = 0;
+        //                 top = 12;
+        //             } else
+        //                 left += 12;
+        //         }
+        //     }
+
+        //     for (var y = 1; y < turn; y++) {
+
+        //         if (player[y].position == x && !player[y].jail) {
+        //             document.getElementById("cell" + x + "positionholder").innerHTML += "<div class='cell-position' title='" + player[y].name + "' style='background-color: " + player[y].color + "; left: " + left + "px; top: " + top + "px;'></div>";
+        //             if (left == 36) {
+        //                 left = 0;
+        //                 top = 12;
+        //             } else
+        //                 left += 12;
+        //         }
+        //     }
+        // }
+
+        // left = 0;
+        // top = 53;
+        // for (var i = turn; i <= pcount; i++) {
+        //     if (player[i].jail) {
+        //         document.getElementById("jailpositionholder").innerHTML += "<div class='cell-position' title='" + player[i].name + "' style='background-color: " + player[i].color + "; left: " + left + "px; top: " + top + "px;'></div>";
+
+        //         if (left === 36) {
+        //             left = 0;
+        //             top = 41;
+        //         } else {
+        //             left += 12;
+        //         }
+        //     }
+        // }
+
+        // for (var i = 1; i < turn; i++) {
+        //     if (player[i].jail) {
+        //         document.getElementById("jailpositionholder").innerHTML += "<div class='cell-position' title='" + player[i].name + "' style='background-color: " + player[i].color + "; left: " + left + "px; top: " + top + "px;'></div>";
+        //         if (left === 36) {
+        //             left = 0;
+        //             top = 41;
+        //         } else
+        //             left += 12;
+        //     }
+        // }
+
+        // p = player[turn];
+
+        // if (p.jail) {
+        //     document.getElementById("jail").style.border = "1px solid " + p.color;
+        // } else {
+        //     document.getElementById("cell" + p.position).style.border = "1px solid " + p.color;
+        // }
+
+        // // for (var i=1; i <= pcount; i++) {
+        // // document.getElementById("enlarge"+player[i].position+"token").innerHTML+="<img src='"+tokenArray[i].src+"' height='30' width='30' />";
+        // // }
+
+    }
+
+
+    updateOwned = () => {
+
+    }
+
+
+    popup = (text, action, option) => {
+        let popupConfig = { show: true } ;
+
+        if(text)    popupConfig.text    =   text;
+        if(action)  popupConfig.action  =   action;
+        if(option)  popupConfig.option  =   option;
+
+        this.props.dispatch(popupActions.setPopupConfig(popupConfig));
+
+    }
+
+
+    gotojail = () => {
+        let p = this.props.playersConfig.players[this.props.game.currentPlayer];
+        this.addAlert(p.name + " was sent directly to jail.");
+
+        let landed={};
+        landed.text="You are in jail.";
+        landed.show=true;
+
+        let nextButton={};
+        nextButton.text="End turn";
+        nextButton.title="End turn and advance to the next player.";
+        nextButton.show=true;
+
+        this.props.dispatch(gameActions.setLanded(landed));
+        this.props.dispatch(gameActions.setNextButton(nextButton));
+
+        p.jail = true;
+        this.setState({doublecount: 0});
+
+        // document.getElementById("nextbutton").value = "End turn";
+        // document.getElementById("nextbutton").title = "End turn and advance to the next player.";
+
+        // if (p.human) {
+        //     document.getElementById("nextbutton").focus();
+        // }
+
+        this.updatePosition();
+        this.updateOwned();
+
+        if (!p.human) {
+            //@TODO !!!
+            this.popup(p.AI.alertList, game.next);
+            p.AI.alertList = "";
+        }
+    }
+
 
     rollDice = () =>{
 
@@ -63,6 +212,9 @@ class ControlBoard extends Component {
 
         config.doublecount++;
 
+        let nextButton={};
+
+
         if (die1 == die2) {
             this.addAlert(p.name + " rolled " + (die1 + die2) + " - doubles.");
         } else {
@@ -73,9 +225,10 @@ class ControlBoard extends Component {
             // updateDice(die1, die2);
 
             if (config.doublecount < 3) {
-                config.nextButton.value = "Roll again";
-                config.nextButton.title = "You threw doubles. Roll again.";
-
+                nextButton.text = "Roll again";
+                nextButton.title = "You threw doubles. Roll again.";
+                nextButton.show = true;
+                this.props.dispatch(gameActions.setNextButton(nextButton));
             // If player rolls doubles three times in a row, send him to jail
             } else if (config.doublecount === 3) {
                 p.jail = true;
@@ -85,22 +238,25 @@ class ControlBoard extends Component {
 
 
                 if (p.human) {
-                    popup("You rolled doubles three times in a row. Go to jail.", goToJail);
+                    this.popup("You rolled doubles three times in a row. Go to jail.", this.gotojail);
                 } else {
-                    gotojail();
+                    this.gotojail();
                 }
 
                 return;
             }
         } else {
-            config.nextButton.value = "End turn";
-            config.nextButton.title = "End turn and advance to the next player.";
+            nextButton.text = "End turn";
+            nextButton.title = "End turn and advance to the next player.";
+            nextButton.show = true;
+            this.props.dispatch(gameActions.setNextButton(nextButton));
+            
             config.doublecount = 0;
         }
 
-        updatePosition();
-        updateMoney();
-        updateOwned();
+        this.updatePosition();
+        this.updateMoney();
+        this.updateOwned();
 
         if (p.jail === true) {
             p.jailroll++;
@@ -109,8 +265,9 @@ class ControlBoard extends Component {
             if (die1 == die2) {
                 // document.getElementById("jail").style.border = "1px solid black";
                 // document.getElementById("cell11").style.border = "2px solid " + p.color;
-                // $("#landed").hide();
-                landed = false;
+        
+
+                this.props.dispatch(gameActions.setLanded({show:false}));
 
                 p.jail = false;
                 p.jailroll = 0;
@@ -124,7 +281,7 @@ class ControlBoard extends Component {
                 if (p.jailroll === 3) {
 
                     if (p.human) {
-                        popup("<p>You must pay the $50 fine.</p>", function() {
+                        this.popup("<p>You must pay the $50 fine.</p>", function() {
                             payFifty();
                             payfifty();
                             p.position =10 + die1 + die2;
@@ -136,11 +293,16 @@ class ControlBoard extends Component {
                         land();
                     }
                 } else {
-                    $("#landed").show();
-                    document.getElementById("landed").innerHTML = "You are in jail.";
+
+                    let landed={};
+                    landed.text="You are in jail.";
+                    landed.show=true;
+
+                    this.props.dispatch(gameActions.setLanded(landed));
+
 
                     if (!p.human) {
-                        popup(p.AI.alertList, game.next);
+                        this.popup(p.AI.alertList, game.next);
                         p.AI.alertList = "";
                     }
                 }
@@ -160,8 +322,11 @@ class ControlBoard extends Component {
                 this.addAlert(p.name + " collected a $200 salary for passing GO.");
             }
 
-            land();
+            // this.land();
         }
+        this.props.dispatch(playerActions.updatePlayer({playerNumber: this.props.game.currentPlayer, playerEntity: p}));
+
+
 
         this.setState(config);
     }   
@@ -169,9 +334,17 @@ class ControlBoard extends Component {
 
     render() {
 
+        let landed;
+        if(this.props.game.landed.show)
+            landed = (
+                <div id="landed" title={this.props.game.landed.title}>
+                    {this.props.game.landed.text}    
+                </div>
+            );
         return (
             <div>
                 <Alert />
+                {landed}
                 <Dice diceNumber={this.props.game.dice.first}/>
                 <Dice diceNumber={this.props.game.dice.second}/>
                 <table>
@@ -182,6 +355,7 @@ class ControlBoard extends Component {
                         />
                     </tbody>
                 </table>
+                <Popup />
                 <Button type="button" className="btn btn-info" onClick={this.rollDice}>Roll dice</Button>
             </div>
         );
@@ -193,7 +367,12 @@ class ControlBoard extends Component {
 
 
 function mapStateToProps(state) {
-    return {playersConfig: state.playersConfig, squareConfig: state.squareConfig, game: state.gameFunctionality};
+    return {
+                playersConfig   : state.playersConfig,
+                squareConfig    : state.squareConfig,
+                game            : state.gameFunctionality,
+                popupConfig     : state.popupConfig
+    };
 }
 
 //connect component with global state
--- src/redux/reducers/gameReducer.js
+++ src/redux/reducers/gameReducer.js
@@ -1,7 +1,14 @@
-import { ROLL_DICE, ADD_ALERT } from '../actions/gameActions';
+import { ROLL_DICE, ADD_ALERT, SET_NEXT_BUTTON, SET_LANDED, UPDATE_P_S } from '../actions/gameActions';
 
 
-const initialState = { playerToSquare: {}, currentPlayer: 0, dice: {}, gameLog:[]};
+const initialState = { 
+						playerToSquare: [], 
+						currentPlayer: 0, 
+						dice: {}, 
+						gameLog:[], 
+						nextButton:{title:'', text:'', show: false},
+						landed: {text:'', show: false}
+};
 
 
 
@@ -13,11 +20,45 @@ export default function(state=initialState, action){
 				dice: action.dice
 			});
 		case ADD_ALERT:
-				state.gameLog.push(action.message);
-				return Object.assign({},{...state});
+			state.gameLog.push(action.message);
+			return Object.assign({},{...state});
+		case SET_NEXT_BUTTON: 
+			return Object.assign({}, {
+				...state,
+			 	nextButton: action.nextButton
+			})
+		case SET_LANDED: 
+			return Object.assign({}, {
+				...state,
+			 	landed: action.landed
+			})
+		case UPDATE_P_S: 
+			let playerToSquare = state.playerToSquare;
+			let index=-1;
+			for(let i in playerToSquare){
+				if(playerToSquare[i].player==action.playerToSquare.player)
+					index=i;
+			}
+			if(index < playerToSquare.length-1)
+				return  Object.assign({},{
+					...state,
+					playerToSquare:[	
+						...state.playerToSquare.slice(0, index-1),
+			    		Object.assign({}, action.playerToSquare),
+			    		...state.playerToSquare.slice(index + 1)
+  					]
+				});
+			else 
+				return  Object.assign({},{
+					...state,
+					playerToSquare:[	
+						...state.playerToSquare.slice(0, index-1),
+			    		Object.assign({}, action.playerToSquare)
+  					]
+				});
+		
 		default:
 			return state;
 	}
 } 
-
 
